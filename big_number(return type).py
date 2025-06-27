0c48da92813a542ee7160946b5cf83a059d3bb30
Reshma Devada <devadareshma@gmail.com>
2025-06-27 08:14:16 +0530

    Add files via upload

---
+++ big_number(return type).py
@@ -0,0 +1,8 @@
+num1=input("Enter an integer:")
+num2=input("Enter another integer:")
+if num1>num2:
+    biggest=num1
+else:
+    biggest=num2
+print("the biggest number is:",biggest)
+print("return type is:",type(biggest))
