fd9a4b11a07b21d78b0af55bf04fd4437a91151b
Reshma Devada <devadareshma@gmail.com>
2025-06-30 14:13:08 +0530

    Add files via upload

---
+++ smallest_among_3.py
@@ -0,0 +1,11 @@
+def main():
+    a=50
+    b=30
+    c=40
+    if a<b and b<c:
+        print("{a} is the smallest number")
+    elif b<c and b<a:
+        print("{b} is the smallest number")
+    else:
+        print("{c} is the smallest number")
+main()
