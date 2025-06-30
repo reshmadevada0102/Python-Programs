aba4bfa0982f6ffebdf5d4b7f399359d039aa162
Reshma Devada <devadareshma@gmail.com>
2025-06-30 14:08:09 +0530

    Add files via upload

---
+++ biggestamongtwo.py
@@ -0,0 +1,9 @@
+def fun():
+    a=int(input("Enter an integer:"))
+    b=int(input("Enter another integer:"))
+    if a>b:
+        print(f"{a} is big")
+    else:
+        print(f"{b} is big")
+if __name__=="__main__":
+    fun()
