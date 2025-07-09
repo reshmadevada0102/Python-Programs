e5bd2b96cf9908591e784738c871fffb8b013182
Reshma Devada <devadareshma@gmail.com>
2025-07-09 22:58:37 +0530

    Add files via upload

---
+++ pattern_8.py
@@ -0,0 +1,12 @@
+def pattern()-> None:
+    for i in range(1,6):
+        for j in range(1,6):
+            if i>j:
+                print('0',end=" ")
+            else:
+                print('1',end=" ")
+        print()
+def main():
+    pattern()
+if __name__=="__main__":
+    main()
