5c71059e839c329cb6988b915326f3b896d64ccd
Reshma Devada <devadareshma@gmail.com>
2025-07-16 17:28:40 +0530

    Add files via upload

--- fact.py
+++ fact.py
@@ -1,7 +1,7 @@
 def fact(n:int)->int:
     result=1
-    for i in range(n,1,n-1):
-        result=(i*i)
+    for i in range(1,n+1):
+        result=result * i
     return result
 def main():
     n=int(input("enter an integer:"))
