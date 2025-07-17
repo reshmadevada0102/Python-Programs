f47aebe0fc811608b0017e978abdfc70e30b8b63
Reshma Devada <devadareshma@gmail.com>
2025-07-17 22:11:27 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+#calculator using 1 line of python code
+
+while True : print(eval(input(">>>")))
+
+#while True : it creates an infinte loop
+#input(">>>") this prompts the user to enter expression. 
+#eval(..)  this function evaluates the user's input as a python expression. If the user enters any arithmetic expressions ,compute it and returns the value.
+#print(...)  the result of eval(input(...)) is printed to the screen.
