be8c1a3859760abd75101fe3502da3ec7ce7644b
Reshma Devada <devadareshma@gmail.com>
2025-07-20 07:01:13 +0530

    Delete pattern_11.py

--- pattern_11.py
+++
@@ -1,9 +0,0 @@
-def pattern()->None:
-    for i in range(5,0,-1):
-        for j in range(5,i-1,-1):
-            print(j,end=" ")      
-        print()
-def main():
-    pattern()
-if __name__=="__main__":
-    main()
