e32325bc52cfbfc74819613d54bc839043f4b8f6
Reshma Devada <devadareshma@gmail.com>
2025-07-21 19:49:24 +0530

    Delete pattern_9.py

--- pattern_9.py
+++
@@ -1,9 +0,0 @@
-def pattern()->None:
-    for i in range(0,5):
-        for j in range(1,6):
-            print((i+j),end=" ")      
-        print()
-def main():
-    pattern()
-if __name__=="__main__":
-    main()
