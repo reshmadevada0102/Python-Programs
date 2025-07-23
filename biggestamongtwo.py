21f81d9dfa0b34d11b028c711879dee5568becc0
Reshma Devada <devadareshma@gmail.com>
2025-07-23 19:04:52 +0530

    Delete biggestamongtwo.py

--- biggestamongtwo.py
+++
@@ -1,9 +0,0 @@
-def fun():
-    a=int(input("Enter an integer:"))
-    b=int(input("Enter another integer:"))
-    if a>b:
-        print(f"{a} is big")
-    else:
-        print(f"{b} is big")
-if __name__=="__main__":
-    fun()
