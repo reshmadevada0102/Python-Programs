85f91f3dc7ac65fe6428080494014c654611f818
Reshma Devada <devadareshma@gmail.com>
2025-08-12 22:44:23 +0530

    Add files via upload

---
+++ complex_o.py
@@ -0,0 +1,13 @@
+class Complex:
+    real = 0
+    imag = 0
+    def __init__(self,r,i):
+        self.real = r
+        self.imag = i
+    def show(self):
+        print(f"{self.real}+i{self.imag}")
+def main():
+    p = Complex(7,4)
+    p.show()
+if __name__ == "__main__":
+    main()
