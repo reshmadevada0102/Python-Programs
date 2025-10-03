5540d8e64917d3c850796481d7a06c231f046a6f
Reshma Devada <devadareshma@gmail.com>
2025-10-03 07:30:29 +0530

    Add files via upload

---
+++ Patterns/pattern_21.py
@@ -0,0 +1,17 @@
+'''
+1 0 1 0 1 
+0 1 0 1 0 
+1 0 1 0 1 
+0 1 0 1 0 
+1 0 1 0 1 
+'''
+
+def pattern():
+    for i in range(1,6):
+        for j in range(1,6):
+            if (i + j) % 2 == 0:
+                print('1', end=" ")
+            else:
+                print('0', end=" ")
+        print()
+pattern()
